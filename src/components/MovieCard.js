ef86e88b56101c7266f3720da8723e13cf636a77
maimuna omar <maymunaabdi07@gmail.com>
2023-05-18 15:45:59 +0300

    done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -15,3 +15,18 @@ function MovieCard({
 }
 
 export default MovieCard;
+
+
+import React from "react";
+
+// const MovieCard = ({ title, posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg", genres }) => {
+//   return (
+//     <div className="movie-card">
+//       <img src={posterSrc} alt={title} />
+//       <h2>{title}</h2>
+//       <small>{genres.join(", ")}</small>
+//     </div>
+//   );
+// };
+
+// export default MovieCard;
